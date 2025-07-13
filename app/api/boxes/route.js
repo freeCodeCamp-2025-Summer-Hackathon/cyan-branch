3ac2472eac5f8c9281aafa6859dfb1fb49ee9f82
Luke Sullivan <luke@sukelully.dev>
2025-07-13 17:44:22 +0100

    fix: fix more conflicts

--- app/api/boxes/route.js
+++ app/api/boxes/route.js
@@ -1,6 +1,6 @@
 import { getServerSession } from "next-auth/next";
 import { NextResponse } from "next/server";
-import { authOptions } from "@/lib/auth_options";
+import { authOptions } from "@/lib/authOptions";
 import { getBoxes } from "@/prisma/queries";
 
 // GET /api/v1/boxes - Get all boxes for the authenticated user
