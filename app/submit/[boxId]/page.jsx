b973ea3b04afa832b37d86b398fefa0248b2c0a2
Luke Sullivan <luke@sukelully.dev>
2025-07-15 14:27:04 +0100

    style: add styling to submission page and improve header item spacing on mobile

--- app/submit/[boxId]/page.jsx
+++ app/submit/[boxId]/page.jsx
@@ -1,5 +1,6 @@
 import { notFound } from "next/navigation";
 import prisma from "@/lib/prisma";
+import styles from "./page.module.css";
 
 export default async function SubmitPage({ params }) {
   const { boxId } = await params;
@@ -13,16 +14,17 @@ export default async function SubmitPage({ params }) {
   }
 
   return (
-    <main>
-      <h1>
-        Submit Feedback for:
-        {box.name}
-      </h1>
-      <form method="POST" action="/api/submissions">
-        <input type="hidden" name="boxId" value={boxId} />
-        <textarea name="message" placeholder="Your feedback..." required />
-        <button type="submit">Submit</button>
-      </form>
+    <main className={styles.main}>
+      <div className={styles.form__container}>
+        <h1 className={styles.title}>
+          {box.name}
+        </h1>
+        <form className={styles.form} method="POST" action="/api/submissions">
+          <input type="hidden" name="boxId" value={boxId} />
+          <textarea className={styles.textarea} name="message" placeholder="Your feedback..." required />
+          <button className={styles.button} type="submit">Submit Anonymously</button>
+        </form>
+      </div>
     </main>
   );
 }
