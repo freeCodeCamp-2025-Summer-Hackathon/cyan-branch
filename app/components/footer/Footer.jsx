5122b713c65e911afe5c99b54d1c385f76dc20bd
MichaelBoateng <michaelboateng064@gmail.com>
2025-07-22 01:48:07 +0000

    Added socials icons

--- app/components/footer/Footer.jsx
+++ app/components/footer/Footer.jsx
@@ -5,10 +5,9 @@ function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.footer__socials}>
-        <SocialsIcon />
-        <SocialsIcon />
-        <SocialsIcon />
-        <SocialsIcon />
+        <SocialsIcon iconName="github" link="https://github.com/freecodecamp" />
+        <SocialsIcon iconName="linkedin" link="https://linkedin.com/freecodecamp" />
+        <SocialsIcon iconName="youtube" link="https://youtube.com/freecodecamp" />
       </div>
     </footer>
   );
--- app/components/footer/SocialsIcon.jsx
+++ app/components/footer/SocialsIcon.jsx
@@ -8,63 +8,65 @@ import {
   FaYoutube,
 } from "react-icons/fa6";
 
-function SocialIcon({ iconName, link }) {
+import styles from "../../styles/footer/SocialsIcon.module.css";
+
+function SocialsIcon({ iconName, link }) {
   if (iconName === "facebook") {
     return (
-      <Link href={link}>
+      <Link href={link} className={styles.socials__icon}>
         <FaFacebook />
       </Link>
     );
   }
   else if (iconName === "instagram") {
     return (
-      <Link href={link}>
+      <Link href={link} className={styles.socials__icon}>
         <FaInstagram />
       </Link>
     );
   }
   else if (iconName === "youtube") {
     return (
-      <Link href={link}>
+      <Link href={link} className={styles.socials__icon}>
         <FaYoutube />
       </Link>
     );
   }
   else if (iconName === "twitter") {
     return (
-      <Link href={link}>
+      <Link href={link} className={styles.socials__icon}>
         <FaXTwitter />
       </Link>
     );
   }
   else if (iconName === "tiktok") {
     return (
-      <Link href={link}>
+      <Link href={link} className={styles.socials__icon}>
         <FaTiktok />
       </Link>
     );
   }
   else if (iconName === "discord") {
     return (
-      <Link href={link}>
+      <Link href={link} className={styles.socials__icon}>
         <FaDiscord />
       </Link>
     );
   }
   else if (iconName === "linkedin") {
     return (
-      <Link href={link}>
+      <Link href={link} className={styles.socials__icon}>
         <FaLinkedin />
       </Link>
     );
   }
   else if (iconName === "github") {
     return (
-      <Link href={link}>
+      <Link href={link} className={styles.socials__icon}>
         <FaGithub />
       </Link>
     );
   }
 };
 
-export default SocialIcon;
+export default SocialsIcon;
