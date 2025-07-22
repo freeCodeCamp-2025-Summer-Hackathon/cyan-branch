b12691c2f2310a4d04457d9355cf60197d3ae1cf
MichaelBoateng <michaelboateng064@gmail.com>
2025-07-22 01:24:33 +0000

    fixed linting issues

--- app/components/footer/SocialIcon.jsx
+++ app/components/footer/SocialIcon.jsx
@@ -5,58 +5,59 @@ import {
   FaLinkedin,
   FaTiktok,
   FaXTwitter,
-  FaYoutube
-} from 'react-icons/fa6';
+  FaYoutube,
+} from "react-icons/fa6";
 
-function SocialIcon ({ iconName, link }){
+function SocialIcon({ iconName, link }) {
   if (iconName === "facebook") {
     return (
       <Link href={link}>
         <FaFacebook />
       </Link>
     );
-  } 
+  }
   else if (iconName === "instagram") {
     return (
       <Link href={link}>
         <FaInstagram />
       </Link>
     );
-  } else if (iconName === "youtube") {
+  }
+  else if (iconName === "youtube") {
     return (
       <Link href={link}>
         <FaYoutube />
       </Link>
     );
-  } 
+  }
   else if (iconName === "twitter") {
     return (
       <Link href={link}>
         <FaXTwitter />
       </Link>
     );
-  } 
+  }
   else if (iconName === "tiktok") {
     return (
       <Link href={link}>
         <FaTiktok />
       </Link>
     );
-  } 
+  }
   else if (iconName === "discord") {
     return (
       <Link href={link}>
         <FaDiscord />
       </Link>
     );
-  } 
+  }
   else if (iconName === "linkedin") {
     return (
       <Link href={link}>
         <FaLinkedin />
       </Link>
     );
-  } 
+  }
   else if (iconName === "github") {
     return (
       <Link href={link}>
