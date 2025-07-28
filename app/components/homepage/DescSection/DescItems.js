f2f5c4f17e683ed7f9010227a6a6fb0477162c3d
JoJo <jonahthwaite@gmail.com>
2025-07-28 12:27:52 +0100

    feat: added text content to homepage

--- app/components/homepage/DescSection/DescItems.js
+++ app/components/homepage/DescSection/DescItems.js
@@ -1,25 +1,31 @@
 const descItems = [
   {
     featName: "Admin signup/login",
-    featDesc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, dignissimos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque commodi maiores vel rem ut quaerat id laudantium dignissimos similique earum. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae reiciendis deleniti alias qui quis dignissimos, tenetur blanditiis nostrum illum doloremque.",
+    featDesc: "Admins can securely sign up and log in to manage their suggestions boxes. The dashboard provides a centralised space to create boxes, view submissions and respond to feedback.",
     imgSrc: "/images/pexels-buro-millennial-636760-1438081.jpg",
     altText: "",
   },
   {
-    featName: "Create suggestion box (name, description, etc)",
-    featDesc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, dignissimos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque commodi maiores vel rem ut quaerat id laudantium dignissimos similique earum. Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+    featName: "Creating a suggestion box",
+    featDesc: "Admins can easily create a new suggestion box by providing a title and description. Each box is designed to collect anonymous feedback on a specific topic or purpose. ",
     imgSrc: "/images/pexels-dvaughnbell-2068664.jpg",
     altText: "",
   },
   {
-    featName: "Generate shareable URL for submissions",
-    featDesc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, dignissimos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque commodi maiores vel rem ut quaerat id laudantium dignissimos similique earum. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae reiciendis deleniti alias qui quis dignissimos, tenetur blanditiis nostrum illum doloremque.",
+    featName: "Generate a shareable URL for submissions",
+    featDesc: "Every suggestion box includes a unique, shareable URL that can be distributed publicly. This link allows anyone to submit suggestions anonymously without the need to log in.",
     imgSrc: "/images/pexels-pixabay-327533.jpg",
     altText: "",
   },
   {
     featName: "View all owned boxes",
-    featDesc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, dignissimos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque commodi maiores vel rem ut quaerat id laudantium dignissimos similique earum.",
+    featDesc: "Admins can see a list of all the suggestion boxes they've created from their dashboard. Each listing includes summary information and quick access to manage or review submissions.",
+    imgSrc: "/images/pexels-pixabay-327533.jpg",
+    altText: "",
+  },
+  {
+    featName: "Public-facing box view",
+    featDesc: "Anyone with access to a box's public URL can read previously submitted suggestions and the admin's replies. This ensures transparency and fosters open communication without compromising anonymity.",
     imgSrc: "/images/pexels-pixabay-327533.jpg",
     altText: "",
   },
